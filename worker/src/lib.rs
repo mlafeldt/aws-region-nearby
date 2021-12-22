6ca2f2d0fe3aa62c505e78279e37db6c98b1dcc3
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2021-12-22 23:43:34 +0100

    Not need for get_async

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -5,7 +5,7 @@ use aws_region_nearby::find_region_nearby;
 #[event(fetch)]
 pub async fn main(req: Request, env: Env) -> Result<Response> {
     Router::new()
-        .get_async("/", |req, _ctx| async move {
+        .get("/", |req, _ctx| {
             let (latitude, longitude) = req.cf().coordinates().unwrap();
             let region = find_region_nearby(latitude, longitude);
             Response::from_json(&region)
