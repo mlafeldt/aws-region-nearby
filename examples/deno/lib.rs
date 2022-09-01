cd9c3a7b6bd395231d2bbbb521eb7a3b1012eca9
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2022-09-01 21:26:19 +0200

    Add Deno example

---
+++ examples/deno/lib.rs
@@ -0,0 +1,19 @@
+use wasm_bindgen::prelude::*;
+use web_sys::{Request, Response};
+
+use aws_region_nearby::*;
+
+#[wasm_bindgen]
+pub async fn handler(_req: Request) -> Result<Response, JsValue> {
+    console_error_panic_hook::set_once();
+
+    let deno_region: DenoRegion = env_var("DENO_REGION").unwrap().parse().unwrap();
+    let aws_region: AwsRegion = deno_region.into();
+
+    Response::new_with_opt_str(Some(&format!("Deno: {deno_region}\nAWS: {aws_region}")))
+}
+
+#[wasm_bindgen(inline_js = "export const env_var = (name) => Deno.env.get(name)")]
+extern "C" {
+    pub fn env_var(key: &str) -> Option<String>;
+}
