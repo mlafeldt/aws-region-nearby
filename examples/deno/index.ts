bba8d17648fad5d7b381598c92e8cf1e3bada66a
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2024-03-06 13:46:28 +0100

    Update Deno example

--- examples/deno/index.ts
+++ examples/deno/index.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.153.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.218.2/http/server.ts";
 import { type HandlerContext, router } from "https://crux.land/router@0.0.12";
 // @deno-types="./build/aws_region_nearby_deno.d.ts"
 import { handler } from "./build/aws_region_nearby_deno.js";
